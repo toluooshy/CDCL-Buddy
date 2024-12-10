f0da7379a745423fecb9a7551582b705d52520b0
toluooshy <56281958+toluooshy@users.noreply.github.com>
2024-12-10 10:31:15 -0500

    UI updates.

--- src/components/Modals/TutorialModal.js
+++ src/components/Modals/TutorialModal.js
@@ -136,8 +136,54 @@ const TutorialModal = ({
     );
   };
 
-  const openInNewTab = (url) => {
-    window.open(url, "_blank", "noreferrer");
+  const getOS = () => {
+    // To dictate how external links are opened
+    const userAgent = navigator.userAgent;
+    let os = null;
+
+    if (/Windows NT 10.0/.test(userAgent)) {
+      os = "Windows 10";
+    } else if (/Windows NT 6.3/.test(userAgent)) {
+      os = "Windows 8.1";
+    } else if (/Windows NT 6.2/.test(userAgent)) {
+      os = "Windows 8";
+    } else if (/Windows NT 6.1/.test(userAgent)) {
+      os = "Windows 7";
+    } else if (/Windows NT 6.0/.test(userAgent)) {
+      os = "Windows Vista";
+    } else if (/Windows NT 5.1/.test(userAgent)) {
+      os = "Windows XP";
+    } else if (/Macintosh/.test(userAgent)) {
+      os = "Mac OS";
+    } else if (/iPhone|iPad|iPod/.test(userAgent)) {
+      os = "iOS";
+    } else if (/Android/.test(userAgent)) {
+      os = "Android";
+    } else if (/Linux/.test(userAgent)) {
+      os = "Linux";
+    }
+
+    return os;
+  };
+
+  const openInNewTab = async (url) => {
+    const os = getOS();
+    console.log(os);
+    if (os === "iOS" || os === "Android") {
+      try {
+        await navigator.clipboard.writeText(url);
+        alert(`URL copied to clipboard:\n${url}`);
+      } catch (err) {
+        console.error("Failed to copy URL to clipboard:", err);
+      }
+      window.open(
+        url,
+        "_blank",
+        "noopener,noreferrer,scrollbars=yes,resizable=yes"
+      );
+    } else {
+      window.open(url, "_blank", "noreferrer");
+    }
   };
 
   return (
